7723e3c811f079f28f446e0983e9f9930fc79f16
lanyimartin98 <55244702+lanyimartin98@users.noreply.github.com>
2020-05-11 21:53:24 +0200

    Logging added.

--- src/main/java/service/CarService.java
+++ src/main/java/service/CarService.java
@@ -8,6 +8,7 @@ import dao.IDAO;
 import exceptions.AnotherFound;
 import exceptions.NotFound;
 import model.Car;
+import org.apache.log4j.Logger;
 import strategies.MakeArrayStrategy;
 import strategies.MakeCarArrayStrategy;
 
@@ -24,6 +25,7 @@ public class CarService {
     private MakeArrayStrategy as;
     private ObjectMapper mapper;
     private TypeReference ref;
+    private Logger logger=Logger.getLogger(CarService.class);
 
     public CarService(){
         this.carDAO=new DAO("Cars","title");
@@ -32,31 +34,39 @@ public class CarService {
         this.mapper.registerModule(new JavaTimeModule());
         this.ref = new TypeReference<ArrayList<Car>>() {
         };
+        logger.info("CarService is initialized");
     }
     //Returns the content of the "Cars" table
     public Collection<Car> getAllData() throws NotFound, IOException, AnotherFound {
+        logger.info("All data of cars were returned.");
         return mapper.readValue(carDAO.getAllData().toString(),ref);
     }
     //Returns the "Car" instance with the given title
     public Car getDataByTitle(String title) throws NotFound, IOException {
         try {
+            logger.info("Data of car under title "+title+" was returned.");
             return mapper.readValue(carDAO.getDataByID(title).toString(), Car.class);
         }catch (NullPointerException e){
+            logger.error("NullPointer exception occured.");
         }
+        logger.error("No car was found under the title "+title+".");
         throw new NotFound();
     }
     //Deletes the instance with the given title
     public void deleteDataByTitle(String title){
         carDAO.deleteByID(title);
+        logger.info("The car under the title "+title+" was deleted.");
     }
     //Specifies the add operation for the DAO.
     public void addData(String obj) throws IOException, NotFound, AnotherFound {
         carDAO.addData(as.MakeAdd(mapper.readValue(obj.toString(),Car.class)));
+        logger.info("Insert of new car completed.");
     }
     //Specifies the modify operation for the DAO.
     public void updateCar(String obj) throws IOException {
         Car car=mapper.readValue(obj.toString(),Car.class);
         carDAO.updateData(as.MakeUpdate(car),car.getTitle());
+        logger.info("The car under the title "+car.getTitle()+" was updated.");
     }
 
 }
