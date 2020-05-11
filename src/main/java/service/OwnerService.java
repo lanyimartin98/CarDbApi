62c6f12aae75202b3ba52cfa4672101ed292ffd7
lanyimartin98 <55244702+lanyimartin98@users.noreply.github.com>
2020-05-11 22:02:17 +0200

    Logging added.

--- src/main/java/service/OwnerService.java
+++ src/main/java/service/OwnerService.java
@@ -7,8 +7,8 @@ import dao.DAO;
 import dao.IDAO;
 import exceptions.AnotherFound;
 import exceptions.NotFound;
-import model.Car;
 import model.Owner;
+import org.apache.log4j.Logger;
 import strategies.MakeArrayStrategy;
 import strategies.MakeOwnerArrayStrategy;
 
@@ -21,6 +21,7 @@ public class OwnerService {
     private MakeArrayStrategy as;
     private ObjectMapper mapper;
     private TypeReference ref;
+    private Logger logger=Logger.getLogger(OwnerService.class);
 
     public OwnerService(){
         this.ownerDAO=new DAO("Owners","id");
@@ -29,31 +30,39 @@ public class OwnerService {
         this.mapper.registerModule(new JavaTimeModule());
         this.ref = new TypeReference<ArrayList<Owner>>() {
         };
+        logger.info("OwnerService initialized.");
     }
     //Returns the content of the "Owners" table
     public Collection<Owner> getAllData() throws NotFound, IOException, AnotherFound {
+        logger.info("All data of owners were returned.");
         return mapper.readValue(ownerDAO.getAllData().toString(),ref);
     }
     //Returns the "Owner" instance with the given id
     public Owner getDataById(String id) throws NotFound, IOException {
         try {
+            logger.info("Data of owner under id:"+id+" was returned.");
             return mapper.readValue(ownerDAO.getDataByID(id).toString(), Owner.class);
         }catch (NullPointerException e){
-            System.out.println("Got it bro!");
+            logger.error("NullPointer exception occured");
         }
+        logger.error("Instance not found under the id:"+id+".");
         throw new NotFound();
+
     }
     //Deletes the instance with the given id
     public void deleteDataByID(String id){
         ownerDAO.deleteByID(id);
+        logger.info("The owner under the id "+id+" was deleted.");
     }
     //Specifies the add operation for the DAO.
     public void addData(String obj) throws IOException, NotFound, AnotherFound {
         ownerDAO.addData(as.MakeAdd(mapper.readValue(obj.toString(),Owner.class)));
+        logger.info("Insert of a new owner performed.");
     }
     //Specifies the modify operation for the DAO.
     public void updateOwner(String obj) throws IOException {
         Owner owner=mapper.readValue(obj.toString(),Owner.class);
         ownerDAO.updateData(as.MakeUpdate(owner),owner.getId());
+        logger.info("The owner under the id "+owner.getId()+" was updated.");
     }
 }
