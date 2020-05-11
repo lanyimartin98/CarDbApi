5b387737a039eaeae3ee2a10c9299759e11439f1
lanyimartin98 <55244702+lanyimartin98@users.noreply.github.com>
2020-05-11 22:21:18 +0200

    Logging added.

--- src/main/java/com/cardb/api/WebController.java
+++ src/main/java/com/cardb/api/WebController.java
@@ -5,6 +5,7 @@ import exceptions.NotFound;
 import model.Car;
 import model.Incident;
 import model.Owner;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.*;
 import service.CarService;
 import service.IncidentService;
@@ -22,33 +23,40 @@ public class WebController {
     CarService carService=new CarService();
     OwnerService ownerService=new OwnerService();
     IncidentService incidentService=new IncidentService();
+    private Logger logger=Logger.getLogger(WebController.class);
 
     @GetMapping("/cars")
     public Collection<Car> getAllData() throws AnotherFound, IOException, NotFound {
+        logger.info("Get request for all cars were made.");
         return carService.getAllData();
     }
     @GetMapping("/owners")
     public Collection<Owner> getAllOwner() throws AnotherFound, IOException, NotFound {
+        logger.info("Get request for all owners were made.");
         return ownerService.getAllData();
     }
 
     @GetMapping("/incidents")
     public Collection<Incident> getAllIncident() throws AnotherFound, IOException, NotFound {
+        logger.info("Get request for all incidents were made.");
         return incidentService.getAllData();
     }
 
     @GetMapping("/car/{title}")
     public Car getCarByTitle(@PathVariable String title) throws NotFound, IOException {
+        logger.info("Get request for the car under the title "+title+" was made.");
         return carService.getDataByTitle(title);
     }
 
     @GetMapping("/owner/{id}")
     public Owner getOwnerById(@PathVariable String id) throws NotFound, IOException {
+        logger.info("Get request for the owner under the id "+id+" was made.");
         return ownerService.getDataById(id);
     }
 
     @GetMapping("/incident/{id}")
     public Incident getIncidentById(@PathVariable String id) throws NotFound, IOException {
+        logger.info("Get request for the incident under the id "+id+" was made.");
         return incidentService.getDataByID(id);
     }
 
@@ -56,33 +64,39 @@ public class WebController {
     @PostMapping("/car/add")
     public void addCar(@RequestBody String json) throws NotFound, AnotherFound, IOException {
         carService.addData(json);
+        logger.info("Post request to add car was performed.");
     }
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/owner/add")
     public void addOwner(@RequestBody String json) throws NotFound, AnotherFound, IOException {
         ownerService.addData(json);
+        logger.info("Post request to add owner was performed.");
     }
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/incident/add")
     public void addIncident(@RequestBody String json) throws NotFound, AnotherFound, IOException {
         incidentService.addData(json);
+        logger.info("Post request to add incident was performed.");
     }
     @CrossOrigin(origins = "http://localhost:3000")
-    @DeleteMapping("/car/delete/{id}")
-    public void deleteCar(@PathVariable String id){
-        carService.deleteDataByTitle(id);
+    @DeleteMapping("/car/delete/{title}")
+    public void deleteCar(@PathVariable String title){
+        carService.deleteDataByTitle(title);
+        logger.info("Delete request made to the car under the title:"+title);
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/owner/delete/{id}")
     public void deleteOwner(@PathVariable String id){
         ownerService.deleteDataByID(id);
+        logger.info("Delete request made to the owner under the id:"+id);
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/incident/delete/{id}")
     public void deleteIncident(@PathVariable String id){
         incidentService.deleteByID(id);
+        logger.info("Delete request made to the incident under the id:"+id);
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
--- src/main/java/service/IncidentService.java
+++ src/main/java/service/IncidentService.java
@@ -7,8 +7,8 @@ import dao.DAO;
 import dao.IDAO;
 import exceptions.AnotherFound;
 import exceptions.NotFound;
-import model.Car;
 import model.Incident;
+import org.apache.log4j.Logger;
 import strategies.MakeArrayStrategy;
 import strategies.MakeIncidentArrayStrategy;
 
@@ -21,6 +21,7 @@ public class IncidentService {
     private MakeArrayStrategy as;
     private ObjectMapper mapper;
     private TypeReference ref;
+    private Logger logger=Logger.getLogger(IncidentService.class);
     public IncidentService(){
         this.incidentDAO=new DAO("Incidents","incident_id");
         this.as=new MakeIncidentArrayStrategy();
@@ -28,32 +29,39 @@ public class IncidentService {
         this.mapper.registerModule(new JavaTimeModule());
         this.ref = new TypeReference<ArrayList<Incident>>() {
         };
+        logger.info("IncidentService initialized.");
     }
     //Returns the content of the "Incidents" table
     public Collection<Incident> getAllData() throws NotFound, AnotherFound, IOException {
+        logger.info("All data of incidents were returned.");
         return mapper.readValue(incidentDAO.getAllData().toString(),ref);
     }
     //Returns the "Incident" instance with the given id
     public Incident getDataByID(String id) throws NotFound, IOException {
         try {
+            logger.info("Data of incident under id:"+id+" was returned.");
             return mapper.readValue(incidentDAO.getDataByID(id).toString(), Incident.class);
         }catch (NullPointerException e){
-
+            logger.error("NullPointerException occured");
         }
+        logger.error("Instance not found under the id:"+id+".");
         throw new NotFound();
     }
     //Specifies the add operation for the DAO.
     public void addData(String obj) throws IOException, NotFound, AnotherFound {
         incidentDAO.addData(as.MakeAdd(mapper.readValue(obj,Incident.class)));
+        logger.info("Insert of a new Incident was performed.");
     }
     //Deletes the instance with the given id
     public void deleteByID(String id){
         incidentDAO.deleteByID(id);
+        logger.info("The incident under the id "+id+" was deleted.");
     }
     //Specifies the modify operation for the DAO.
     public void updateIncident(String obj) throws IOException {
         Incident inc=mapper.readValue(obj.toString(),Incident.class);
         incidentDAO.updateData(as.MakeUpdate(inc),String.valueOf(inc.getIncident_id()));
+        logger.info("The incident under the id "+inc.getIncident_id()+" was updated.");
     }
 }
 
